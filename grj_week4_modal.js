96f3e9d56b37d7d0e9a416d7c7dd9628d9c80d33
soyi <myeongsoyi@gmail.com>
2022-05-23 11:40:48 +0900

    week4 try1

---
+++ grj_week4_modal.js
@@ -0,0 +1,9 @@
+document.getElementById("modal").style.display = "none";
+
+document.getElementById("addbtn").onclick = function () {
+    document.getElementById("modal").style.display = "block";
+}
+//배경 어둡게 어떻게 해
+document.getElementById("closewx").onclick = function () {
+    document.getElementById("modal").style.display = "none";
+}
